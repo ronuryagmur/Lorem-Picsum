b29247503b716f6d5596a3c7b20c71099a2c2017
Onur <ronuryagmur@gmail.com>
2024-03-17 14:23:09 +0300

    first commit

---
+++ app/src/main/java/com/onur/lorempicsum/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.onur.lorempicsum
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+//        val navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.fragment_Container_view) as NavHostFragment
+//        val navController = navHostFragment.navController
+    }
+}
